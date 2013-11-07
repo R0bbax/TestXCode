80fec278976c03bc918f83ae5beecf560a45b0cd
Robbert Bax <robbertbax@air.robbert.private>
2013-11-07 15:57:10 +0100

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Robbert Bax on 07-11-13.
+//  Copyright (c) 2013 Robbert Bax. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
